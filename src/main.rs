4b4b4959c2735e0b13c5f5ef038ac0b38f49489c
NielDuysters <66119889+NielDuysters@users.noreply.github.com>
2022-05-31 00:33:17 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ mod tigron_sms;
 #[tokio::main]
 async fn main() {
     let tigron_sms = tigron_sms::TigronSms {
-        credentials: ("nieltest".to_string(), "parfaz-basziv-daqcE6".to_string()),
+        credentials: ("".to_string(), "".to_string()),
     };
 
-    let to = "+32.479374727".to_string();
-    let from = "+32.479374727".to_string();
+    let to = "".to_string();
+    let from = "".to_string();
     let message = "test".to_string();
 
     tigron_sms.send(to, from, message).await;
